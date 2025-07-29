5f97c1dc36935d3c4af537b6543431a91c894467
Gyasuddinali <gyasuddinali0786@gmail.com>
2025-07-29 17:33:00 +0530

    call all file in app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import Header from './components/Header';
 import DashboardMainContent from './components/DashboardMainContent';
 import Photo from './photography/Photo';
 import RootLayout from './tour/RootLayout';
+import AboutSection from "./tour/AboutSection";
+import MountainRom from "./tour/MountainRom";
+import Profile from "./tour/Profile";
+import Natural from "./tour/Natural";
+import Himalay from './tour/Himalay'
 function App() {
   return (
 
 
  <div>
-  <RootLayout></RootLayout>
+  {/* <RootLayout></RootLayout> */}
+  <Profile></Profile>
+  <Himalay></Himalay>
+  <MountainRom></MountainRom>
+  <AboutSection></AboutSection>
       {/* <Sidebar />
       <div className="flex-grow-1 d-flex flex-column">
         <Header />
