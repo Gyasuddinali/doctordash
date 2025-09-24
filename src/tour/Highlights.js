9f64259ec7ae32f271fa1e37e012f8f899fc6b15
Gyasuddinali <gyasuddinali0786@gmail.com>
2025-09-24 18:04:08 +0530

    highlight content change

--- src/tour/Highlights.js
+++ src/tour/Highlights.js
@@ -3,15 +3,26 @@
 
 import "./highlight.css"; 
 function Highlights() {
-  const highlights = [
-    "Breathtaking Views –  See Nandadevi, Trishul, Panchachuli from your room",
-    "Kasardevi Temple –  A spiritual landmark surrounded by oak forests",
-    "Tranquil Retreat –  Inhabited by Buddhist monks; ideal for meditation & long stays",
-    "Well Connected – Excellent public transport to & from Almorah",
-    "Nature All Around – Oak forests, nature walks, and serene picnic spots",
-    "Perfect Location –  Far from the chaos, close to amenities",
-  ];
+  // const highlights = [
+  //   "Breathtaking Views –  See Nandadevi, Trishul, Panchachuli from your room",
+  //   "Kasardevi Temple –  A spiritual landmark surrounded by oak forests",
+  //   "Tranquil Retreat –  Inhabited by Buddhist monks; ideal for meditation & long stays",
+  //   "Well Connected – Excellent public transport to & from Almorah",
+  //   "Nature All Around – Oak forests, nature walks, and serene picnic spots",
+  //   "Perfect Location –  Far from the chaos, close to amenities",
+  // ];
 
+
+  const highlights=[
+"Highest hill of Almorah — 6500 ft. above sea level", 
+ "Just 6 km from Almorah town",
+ "Uninterrupted views of Nandadevi, Trishul, and Panchachuli", 
+ "Away from the chaos of cities yet close to modern amenities",
+ "Inhabited by Buddhist monks, popular for long stays" ,
+ "Home to Kasardevi temple, surrounded by dense oak forests", 
+"Excellent public transport to and from Almora" ,
+
+  ]
   return (
     <section className="highlights-section">
       <div className="container">
